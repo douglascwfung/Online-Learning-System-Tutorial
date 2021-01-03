7a14203bbb9b9d9a673e8bc85346bd014cf9d64c
dougl <dougl@LAPTOP-S45BDKLO>
2021-01-03 16:29:27 +0800

    First Commit

---
+++ Online-Learning-System-Tutorial/src/main/java/net/icestone/ols/OnlineLearningSystemTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.icestone.ols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLearningSystemTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLearningSystemTutorialApplication.class, args);
+	}
+
+}
